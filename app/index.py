e053c6f659a41a7c54ccb44eaf967cb0e499abf9
NhuongNhuong0206 <nguyenthihienvy0901@gmail.com>
2023-12-31 14:57:43 +0700

    create login

--- app/index.py
+++ app/index.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, jsonify, session
+from flask import render_template, request, redirect,url_for, jsonify, session
 from app import app, util
 from validate_email import validate_email
 from datetime import datetime
@@ -8,10 +8,22 @@ def index():
     return render_template('homeAndFindFlights.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['get', 'post'])
 def login():
+    er_m_num = 0
+    er_m_tex = ''
+    if request.method.__eq__('POST'):
 
-    return render_template('signIn.html')
+        email = request.form.get('email')
+        passw1 = request.form.get('passw1')
+        user_1 = util.check_login(email=email, passw1=passw1)
+        print(user_1)
+        if user_1:
+            return render_template('homeAndFindFlights.html', er_m_num=er_m_num, er_m_tex=er_m_tex)
+        else:
+            er_m_num = 1
+            er_m_tex = 'Lỗi'
+    return render_template('signIn.html', er_m_num=er_m_num, er_m_tex=er_m_tex)
 
 
 @app.route('/logup', methods=['get', 'post'])
@@ -55,27 +67,32 @@ def logup():
                 er_m_num = 4
                 er_m_tex = 'Bạn chưa nhập Mật khẩu hoặc Mật khẩu không khớp'
             else:
-                for data in util.DuLieuSoDinhDanh():
-                    if data.__eq__(identification):
+                for dataI in util.DuLieuSoDinhDanh():
+                    if dataI.__eq__(identification):
                         er_m_num = 8
                         er_m_tex = 'Mã số định danh đã được đăng kí tài khoản, hãy đăng nhập'
+                        print("Mã số định danh trùng")
                         break
-                for data in util.DuLieuEmail():
-                    if data.__eq__(email):
+
+                for dataE in util.DuLieuEmail():
+                    if dataE.__eq__(email):
                         er_m_num = 9
                         er_m_tex = 'Email đã được đăng kí tài khoản, hãy đăng nhập'
+                        print("Email trùng")
                         break
-                if not er_m_num.__eq__(8):
+                if not (er_m_num.__eq__(8) or er_m_num.__eq__(9)):
+                    print("Không được vào")
                     if len(passw1) >= 8:
                         util.add_user(name=name, passw1=passw1, email=email, birthdate=birthdate, address=address,
                                       identification=identification, nationality=nationality, phone=phone)
+                        print("vào tạo")
                         return redirect('/login')
                     else:
                         er_m_num = 6
                         er_m_tex = 'Mật khẩu phải có ít nhất 8 kí tự'
-                        print('lỗi nè')
-
+                        print('Không vào tạo')
         except Exception as ex:
+            print('lỗi server')
             er_m = 'Lỗi Server'
             er = 0
 
